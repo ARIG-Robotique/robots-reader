2af56c18c1ab1836bd2ae5f431b33fa4c6fdc92c
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2017-08-07 23:35:43 +0200

    Ajout de la configuration des robots en relatif (on as tous le même wksp)

---
+++ flyway-4.2.0/sql/V1_1__add-robots.sql
@@ -0,0 +1,4 @@
+-- Insertion des Robots (en relatif, mais on as tous le même workspace ;-) )
+INSERT INTO robots.robots (name, dir) VALUES
+    ('Nerell robot', '../../java/robots/nerell-parent/nerell-robot/logs'),
+    ('Nerell simulateur', '../../java/robots/nerell-parent/nerell-simulator/logs');
