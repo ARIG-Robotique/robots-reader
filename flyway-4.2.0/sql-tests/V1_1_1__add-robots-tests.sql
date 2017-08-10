80b330949f5d0fbbc13f423d5e142730d53a88db
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2017-08-10 19:56:59 +0200

    Change test robot ID

--- flyway-4.2.0/sql-tests/V1_1_1__add-robots-tests.sql
+++ flyway-4.2.0/sql-tests/V1_1_1__add-robots-tests.sql
@@ -1,2 +1,2 @@
 -- Insertion du robot pour les tests CI
-INSERT INTO robots.robots (id, name, dir) VALUES (-1, 'Test robot', './tests/logs');
+INSERT INTO robots.robots (id, name, dir) VALUES (0, 'Test robot', './tests/logs');
