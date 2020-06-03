a8f2a01a7020930265128c7d89ad86d29694fdee
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2020-06-03 14:03:58 +0200

    Ajout de l'identifiant du robot dans le DTO

--- src/dto/ExecsDTO.ts
+++ src/dto/ExecsDTO.ts
@@ -2,11 +2,13 @@ import {Exec} from '../models/Exec';
 
 export class ExecsDTO {
     id: string;
+    idRobot: number;
     dateStart: Date;
     dateEnd: Date;
 
     constructor(exec?: Exec) {
         this.id = exec.id;
+        this.idRobot = exec.idRobot;
         this.dateStart = exec.dateStart;
         this.dateEnd = exec.dateEnd;
     }
